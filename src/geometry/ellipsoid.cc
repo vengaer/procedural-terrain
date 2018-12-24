9e52b0badb5aada0e9ed5afbbf23b7a92e966376
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2018-12-24 19:21:42 +0100

    Indices correct

--- src/geometry/ellipsoid.cc
+++ src/geometry/ellipsoid.cc
@@ -6,15 +6,17 @@ Ellipsoid::Ellipsoid(GLuint vertical_segments, GLuint horizontal_segments, GLflo
 
 void Ellipsoid::init(GLuint vertical_segments, GLuint horizontal_segments, GLfloat x_scale, GLfloat y_scale, GLfloat z_scale) {
 	/* Prevent potential overflow */
-	glm::clamp(vertical_segments, 2u, 30u);
-	glm::clamp(horizontal_segments, 2u, 30u);
+	glm::clamp(vertical_segments, 5u, 30u);
+	glm::clamp(horizontal_segments, 5u, 30u);
 	glm::clamp(x_scale, 0.1f, 2.f);
 	glm::clamp(y_scale, 0.1f, 2.f);
 	glm::clamp(z_scale, 0.1f, 2.f);
 
 	/* Top, bottom, vertical_segments - 1 for remaining vertical vertices, 
  	 * horizontal_segments + 1 for horizontal vertices*/
-	vertices_.reserve(VERTEX_SIZE * (2 + (vertical_segments - 1) * (horizontal_segments + 1)));
+	auto const NUMBER_OF_VERTICES = 2 + (vertical_segments - 1) * horizontal_segments;
+
+	vertices_.reserve(VERTEX_SIZE * NUMBER_OF_VERTICES);
 	
 	{
 		using std::sin;
@@ -45,7 +47,7 @@ void Ellipsoid::init(GLuint vertical_segments, GLuint horizontal_segments, GLflo
 			y = cos(theta);
 			sin_theta = sin(theta);
 
-			for(auto j = 0u; j < horizontal_segments + 1; j++){
+			for(auto j = 0u; j < horizontal_segments ; j++){
 				s = interpolation::linear(static_cast<GLfloat>(j) / static_cast<GLfloat>(horizontal_segments));
 				
 				phi = 2.f * PI * static_cast<GLfloat>(j) / static_cast<GLfloat>(horizontal_segments);
@@ -56,9 +58,9 @@ void Ellipsoid::init(GLuint vertical_segments, GLuint horizontal_segments, GLflo
 				vertex[0] = x_scale * x; /* Position */
 				vertex[1] = y_scale * y;
 				vertex[2] = z_scale * z;
-				vertex[3] = x / abs(x);  /* Normal */
-				vertex[4] = y / abs(y);
-				vertex[5] = z / abs(z);
+				vertex[3] = x;  /* Normal */
+				vertex[4] = y;
+				vertex[5] = z;
 				vertex[6] = s;			 /* Texture */
 				vertex[7] = t;
 	
@@ -78,16 +80,63 @@ void Ellipsoid::init(GLuint vertical_segments, GLuint horizontal_segments, GLflo
 		
 		vertices_.insert(std::end(vertices_), std::begin(vertex), std::end(vertex));
 	}
-
 	/* 2 * horizontal_segments for triangles connected to top and bottom vertices,
  	 * vertical_segments - 2 for remaining vertical segments and horizontal_segments
  	 * for remainig horizontal vertices. *2 for 2 triangles / quad */
 	indices_.reserve(3 * 2 * (horizontal_segments + (vertical_segments - 2) * horizontal_segments));
 
 	std::array<GLuint, 3> triangle_indices;
-	/* TODO: indices */
 
+	/* Top vertex */
+	for(auto i = 0u; i < horizontal_segments; i++){
+		triangle_indices[0] = 0u;
+		triangle_indices[1] = i+1;	
+		triangle_indices[2] = i+2;
+		
+		/* Last vertex in final triangle */
+		if(triangle_indices[2] > horizontal_segments)
+			triangle_indices[2] -= horizontal_segments;
 
+		indices_.insert(std::end(indices_), std::begin(triangle_indices), std::end(triangle_indices));
+	}
+
+	auto idx = 1u;
+	for(auto i = 0u; i < vertical_segments-2; i++) {
+		for(auto j = 0u; j < 2*horizontal_segments; j++) {
+			triangle_indices[0] = idx;
+			if(j % 2){
+				triangle_indices[1] = idx - 1;
+				triangle_indices[2] = idx + horizontal_segments;
+			}
+			else {
+				triangle_indices[1] = idx + horizontal_segments;
+				triangle_indices[2] = idx + horizontal_segments + 1;
+				idx++;
+			}
+
+		/* Triangles for last quad in each horizontal belt */
+		if((triangle_indices[2] % horizontal_segments) == 1){
+			triangle_indices[2] -= horizontal_segments;
+			if((triangle_indices[0] % horizontal_segments) == 1)
+				triangle_indices[0] -= horizontal_segments;
+		}
+
+		indices_.insert(std::end(indices_), std::begin(triangle_indices), std::end(triangle_indices));
+		}
+	}
+
+	/* Bottom vertex */
+	for(auto i = 0u; i < horizontal_segments; i++){	
+		triangle_indices[0] = NUMBER_OF_VERTICES - 1;
+		triangle_indices[1] = NUMBER_OF_VERTICES - 2 - i;
+		triangle_indices[2] = NUMBER_OF_VERTICES - 3 - i;
+
+		/* Last vertex in final triangle */
+		if(triangle_indices[2] < NUMBER_OF_VERTICES - horizontal_segments - 1)
+			triangle_indices[2] += horizontal_segments;
+
+		indices_.insert(std::end(indices_), std::begin(triangle_indices), std::end(triangle_indices));
+	}
 }
 
 std::vector<GLfloat> const& Ellipsoid::vertices() const {
