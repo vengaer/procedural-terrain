964f4d803e4da73a5d1ab4f8c64bb6fd7a696c38
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2018-12-25 17:12:39 +0100

    Split interpolation into 2 files

--- src/math/interpolation.h
+++ src/math/interpolation.h
@@ -9,30 +9,20 @@
 
 namespace interpolation{
 	inline double constexpr PI = 3.14159265359;
-
+	
 	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
 	struct Parameters {
 		T x;
 		T y;
 	};
-	
-	template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-	inline T linear(T t, T min = static_cast<T>(0.0), T max = static_cast<T>(1.0)){
-		glm::clamp(t, min, max);
-		
-		return (1-t)*min + t*max;
-	}
 
 	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-	inline Parameters<T> polar(T angle, T radius = static_cast<T>(1.0)){
-		angle = std::fmod(angle, 2*PI);
+	T linear(T t, T min = static_cast<T>(0.0), T max = static_cast<T>(1.0));
 
-		Parameters<T> result;
-		result.x = radius * std::cos(angle);
-		result.y = radius * std::sin(angle);
-	
-		return result;
-	}
+	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+	Parameters<T> polar(T angle, T radius = static_cast<T>(1.0));
 }
+
+#include "interpolation.tcc"
 
 #endif
---
+++ src/math/interpolation.tcc
@@ -0,0 +1,16 @@
+
+template<typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+T interpolation::linear(T t, T min, T max){
+	glm::clamp(t, min, max);
+	
+	return (1-t)*min + t*max;
+}
+
+template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+interpolation::Parameters<T> interpolation::polar(T angle, T radius){
+	Parameters<T> result;
+	result.x = radius * std::cos(angle);
+	result.y = radius * std::sin(angle);
+
+	return result;
+}
