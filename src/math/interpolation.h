134a06f96688a97f0f5ba129fba19a177c9a5210
VilhelmEngstrom <vilhelmengstrom@tuta.io>
2018-12-31 13:45:42 +0100

    Decaying types properly

--- src/math/interpolation.h
+++ src/math/interpolation.h
@@ -11,17 +11,17 @@
 namespace interpolation{
 	inline double constexpr PI = 3.14159265359;
 	
-	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<remove_cvref_t<T>>>>
+	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<std::decay_t<T>>>>
 	struct Parameters {
-		T x;
-		T y;
+		std::decay_t<T> x;
+		std::decay_t<T> y;
 	};
 
-	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<remove_cvref_t<T>>>>
+	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<std::decay_t<T>>>>
 	std::decay_t<T> linear(T t, T min = static_cast<T>(0.0), T max = static_cast<T>(1.0));
 
-	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
-	Parameters<std::decay_t<T>> polar(T angle, T radius = static_cast<T>(1.0));
+	template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<std::decay_t<T>>>>
+	Parameters<T> polar(T angle, T radius = static_cast<T>(1.0));
 }
 
 #include "interpolation.tcc"
