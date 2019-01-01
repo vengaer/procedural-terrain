1ea6346e55684f9f809f3c70a06b0b703ad55b93
VilhelmEngstrom <vilhelmengstrom@tuta.io>
2019-01-01 15:01:57 +0100

    Separate files for constants

---
+++ src/math/constants.cc
@@ -0,0 +1,6 @@
+#include "constants.h"
+
+inline namespace constants {
+	double const PI = 3.14159265359;
+	double const e  = 2.71828182845;
+}
---
+++ src/math/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONTANST_H
+
+#pragma once
+
+inline namespace constants {
+	extern double const PI;
+	extern double const e;
+}
+
+#endif
