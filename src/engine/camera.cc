4a48a84ff57a0d0423c54a645a6c737b55e8966c
VilhelmEngstrom <vilhelm.engstrom@tuta.io>
2019-01-03 19:28:24 +0100

    Translation on callback

--- src/engine/camera.cc
+++ src/engine/camera.cc
@@ -1,10 +1,10 @@
 #include "camera.h"
 
-Camera::Camera(std::size_t window_width, std::size_t window_height, glm::vec3 position, glm::vec3 target_view, float fov) : position_{position}, local_x_{}, local_y_{}, local_z_{}, view_{}, perspective_{}, fov_{fov} { 
-	init(window_width, window_height, target_view);
+Camera::Camera(glm::vec3 position, glm::vec3 target_view, float fov, ClippingPlane plane) : position_{position}, local_x_{}, local_y_{}, local_z_{}, view_{}, fov_{fov}, clipping_plane_{plane} { 
+	init(target_view);
 }
 
-void Camera::init(std::size_t window_width, std::size_t window_height, glm::vec3 target_view) {
+void Camera::init(glm::vec3 target_view) {
 	local_z_ = glm::normalize(position_ - target_view);
 
 	glm::vec3 world_y = glm::vec3{0.f, 1.f, 0.f};
@@ -14,9 +14,33 @@ void Camera::init(std::size_t window_width, std::size_t window_height, glm::vec3
 
 	view_ = glm::lookAt(position_, position_ - local_z_, world_y);
 
-	perspective_ = glm::perspective(glm::radians(fov_), static_cast<float>(window_width)/static_cast<float>(window_height), 0.1f, 100.f);
 }
 
-glm::mat4 Camera::get_perspective() const {
-	return perspective_;
+void Camera::translate(Direction dir, Speed speed) {
+	int sign = std::pow(-1, enum_value(dir));
+	float speed_modifier = static_cast<float>(speed) / 10.f * frametime::delta();
+	float increment = sign * speed_modifier;
+	glm::mat4 translation_matrix{1.f};
+
+	switch(enum_value(dir) / 2) {
+		case 0: 
+			translation_matrix = glm::translate(translation_matrix, increment * local_x_);
+			break;
+		case 1:
+			translation_matrix = glm::translate(translation_matrix, increment * local_y_);
+			break;
+		case 2:
+			translation_matrix = glm::translate(translation_matrix, increment * local_z_);
+			break;
+	}
+
+	position_ = translation_matrix * glm::vec4{position_, 1.f};
+}
+
+float Camera::fov() const {
+	return fov_;
+}
+
+ClippingPlane Camera::clipping_plane() const {
+	return clipping_plane_;
 }
